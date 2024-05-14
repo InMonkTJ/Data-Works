3aaeced7c9ccbbbbb8538d91ba4bbac781b67139
= <=>
2024-05-14 22:36:09 +0100

    kk

--- azure/to_sql.py
+++ azure/to_sql.py
@@ -19,10 +19,9 @@ connection = (
     f"mssql+pyodbc://{username}:{password}@{db_config['server']}/{db_config['database']}?driver={driver}&Command Timeout=120&connect_timeout=600"
 )
 
-
+engine = create_engine(connection, echo=True)
 
 def data_to_sql(data):
-    engine = create_engine(connection, echo=True)
     df = pd.DataFrame(data)
 
     df.to_sql("stocks", con=engine,index=False, if_exists='append')
