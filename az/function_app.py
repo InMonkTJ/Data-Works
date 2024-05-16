3c7a4bc23bbb71dbdf52863c3407f830c2874c1d
= <=>
2024-05-16 18:11:43 +0100

    dsdd

--- az/function_app.py
+++ az/function_app.py
@@ -4,80 +4,68 @@ import requests
 import pandas as pd
 import yaml
 from to_sql import data_to_sql
-import argparse
 import datetime
 
-
-
-
-
-path_to_file = "../lib/cc.yaml"
+path_to_file = "shared/cc.yaml"
 
 def parse_date(date_string):
     return datetime.datetime.strptime(date_string, "%d/%m/%Y")
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--startfrom', type=parse_date, default=datetime.datetime.now() - datetime.timedelta(days=2), help= "where do you want to start from")
-parser.add_argument('-e', '--endto', type=parse_date, default=datetime.datetime.now(), help= "where do you want to end to")
-
-args = parser.parse_args()
+# Set default values for start and end dates
+start_date = datetime.datetime.now() - datetime.timedelta(days=2)
+end_date = datetime.datetime.now()
 
 with open(path_to_file, "r") as file:
     config = yaml.safe_load(file)
 
-# Get the api key and the stocks to bulk import
+# Get the API key and the stocks to bulk import
 api_key = config["Api"]["Alphavantage"]
 stocks = config["Companies_bulk"]["Names"]
 
-
-def get_historic_data(api_key, stocks, args):
+def get_historic_data(api_key, stocks, start_date, end_date):
     all_rows = []
-    for company_names, stock in stocks.items(): #Iterate over each company
-        req = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey={}&outputsize=full'.format(stock, api_key)) #Api connection url
+    for company_names, stock in stocks.items():
+        req = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey={api_key}&outputsize=full')
         data_json = req.json()
-        daily_data = data_json["Time Series (Daily)"]
-        
-        for dates, info in daily_data.items(): # Access daily trading stats, append the rows, convert all the rows into dataframe
-            day_row = { "company": company_names,
-                       "stock": stock,
-                        "trading_day" : dates,
-                        "open_price" : info['1. open'],
-                        "high_price" : info['2. high'],
-                        "low_price": info['3. low'],
-                        "close_price": info['4. close'],
-                        "volume": info['5. volume']
-                    }
+        daily_data = data_json.get("Time Series (Daily)", {})
+
+        for dates, info in daily_data.items():
+            day_row = {
+                "company": company_names,
+                "stock": stock,
+                "trading_day": dates,
+                "open_price": info['1. open'],
+                "high_price": info['2. high'],
+                "low_price": info['3. low'],
+                "close_price": info['4. close'],
+                "volume": info['5. volume']
+            }
             all_rows.append(day_row)
+    
     df = pd.DataFrame(all_rows)
 
     # Convert 'trading_day' to datetime for accurate filtering and comparison
     df['trading_day'] = pd.to_datetime(df['trading_day'])
 
-    if args.startfrom:
-        df = df[(df['trading_day'] >= args.startfrom) & (df['trading_day'] <= args.endto)]
+    if start_date:
+        df = df[(df['trading_day'] >= start_date) & (df['trading_day'] <= end_date)]
 
-    # Change the datatype of the columns and ensure the change is applied
-    df = df.astype({"company": 'category', "stock": 'category', 'open_price': 'float', 'high_price': 'float', 'low_price': 'float', 'close_price': 'float', 'volume': 'int'})
-    
-    # Import to the database
+    df = df.astype({
+        "company": 'category',
+        "stock": 'category',
+        'open_price': 'float',
+        'high_price': 'float',
+        'low_price': 'float',
+        'close_price': 'float',
+        'volume': 'int'
+    })
+
     data_to_sql(df)
-    
 
-
-
-
-
-
-
-
-app = func.FunctionApp()
-
-@app.schedule(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
-def timer_trigger(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
+def main(mytimer: func.TimerRequest) -> None:
+    if mytimer.past_due:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
     
-    get_historic_data(api_key, stocks, args)
+    get_historic_data(api_key, stocks, start_date, end_date)
