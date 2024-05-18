92db4249640f4dc06b5c2589f6affb5408f54254
= <=>
2024-05-18 21:02:01 +0100

    dsds

---
+++ az/to_sql.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import yaml
+
+# Load YAML configuration
+with open('TimerTrigger1/dd.yaml', "r") as file:
+    config = yaml.safe_load(file)
+
+# Extract database configuration
+db_config = config['Sql_connections']['sen_data']
+
+username = db_config['username']
+password = db_config['password']
+server = db_config['server']
+database = db_config['database']
+
+# Create connection string using pymssql
+connection = (
+    f"mssql+pymssql://{username}:{password}@{server}/{database}?timeout=600"
+)
+
+# Create SQLAlchemy engine
+engine = create_engine(connection, echo=True)
+
+def data_to_sql(data, n):
+    # Convert data to DataFrame
+    df = pd.DataFrame(data)
+    
+    # Insert data into the "stocks" table
+    df.to_sql("stocks", con=engine, index=False, if_exists='append')
